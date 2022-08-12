096a059a574349ebabb0827219189a43f6492dcb
Jake Harris <jakec.harris@gmail.com>
2022-08-12 14:37:39 -0500

    new file:   fastq_phred.txt 	modified:   scratch.py

--- scratch.py
+++ scratch.py
@@ -1,5 +1,6 @@
 from doctest import OutputChecker
 from Bio import Entrez, SeqIO
+from Bio.Seq import translate
 import numpy as np
 Entrez.email = 'jakec.harris@gmail.com'
 # handle = Entrez.esearch(db='nucleotide', term='"Dascyllus"[Organism] AND ("2000/04/06"[PDAT] : "2011/03/22"[PDAT])')
@@ -39,7 +40,30 @@ def fastaq_conv(fastq):
             # print(record.id, record.seq)
             print(SeqIO.FastaIO.as_fasta_2line(record))
 
+def phred(fastq):
+    with open(fastq, 'r') as f:
+        score = f.readline().rstrip()
+        fq_records = f.readlines()
+    with open('fastq_phred.txt', 'w+') as fq:
+        fq.writelines(fq_records)
+    below_score = 0
+    for rec in SeqIO.parse('fastq_phred.txt', 'fastq'):
+        phred_mean = np.mean(rec.letter_annotations['phred_quality'])
+        if phred_mean < int(score):
+            below_score += 1
+    print(below_score)
+
+def prot_translate(dna_file):
+    with open(dna_file) as f:
+        dna = f.readline().strip()
+        prot = f.readline().strip()
+    # print(dna.find('ATG'))
+    print(len(translate(dna)), len(prot))
+
+    
+
 if __name__ == "__main__":
     # shortest(["FJ817486", "JX069768", "JX469983"])
     # shortest(['NM_001081821 JX445144 NM_001079732 NM_214399 NM_001185098 JX469983 JX308813 JX472277 JF927157'])
-    fastaq_conv('/Users/jakeharris/Downloads/rosalind_tfsq.txt')
+    # phred('/Users/jakeharris/Downloads/rosalind_phre.txt')
+    prot_translate('/Users/jakeharris/Downloads/rosalind_ptra (1).txt')
